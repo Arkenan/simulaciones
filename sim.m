47e2a1f695f7853d3db472f2017c49d42c0b87a7
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-10-09 15:40:52 -0300

    cambio para aceptar otras funciones

--- sim.m
+++ sim.m
@@ -7,12 +7,13 @@ pasoX = topeX/100;
 T = (2*pi)/c;
 pasoT = T/100;
 x = [0:pasoX:topeX];
-function y = s1(x,t,c)
+
+function y = senoidal(x,t,c)
   y = 0.5 * ( sin(x + c*t) + sin(x - c*t));
 endfunction
 
 % Para pasar handler
-f = @(x,t) s1(x,t,c);
+f = @(x,t) senoidal(x,t,c);
 
 for t = [0:pasoT:T]
   f(x,t)
