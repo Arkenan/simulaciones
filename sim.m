3117bd36a2e105da42bb93d786a69b646bf3b202
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-10-09 15:57:07 -0300

    listo para funciones genericas. Se agrega funcion 'estacionaria' como dos ondas viajeras en sentido contrario.

--- sim.m
+++ sim.m
@@ -1,6 +1,17 @@
 clear;
 cd output
 
+function y = triangular(x,h,L)
+  % Hay que separar en Periodos de subida y de bajada. 
+  x = mod(x,L);
+  esSubida = (x <= L/2);
+  y = (2*h/L*x).*esSubida + (2*h - 2*h/L*x).*(1-esSubida);
+endfunction
+
+function f = estacionaria(onda,c)
+  f = @(x,t) 1/2*(onda(x + c*t) + onda(x - c*t));
+endfunction
+
 c = 1;
 topeX = pi;
 pasoX = topeX/100;
@@ -8,16 +19,12 @@ T = (2*pi)/c;
 pasoT = T/100;
 x = [0:pasoX:topeX];
 
-function y = senoidal(x,t,c)
-  y = 0.5 * ( sin(x + c*t) + sin(x - c*t));
-endfunction
-
 % Para pasar handler
-f = @(x,t) senoidal(x,t,c);
+f1 = estacionaria(@sin,c)
+f2 = estacionaria(@(x) triangular(x,1,pi),c)
 
 for t = [0:pasoT:T]
-  f(x,t)
-  plot( x, f(x,t),"." );
+  plot( x, f1(x,t),x,f2(x,t),"." );
   axis([0,topeX,-1,1]);
   nombreImagen = sprintf("%05d.png",t/pasoT);
   print("-dpng",nombreImagen);
