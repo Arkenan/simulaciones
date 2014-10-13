5415ab325e015d8f7edc66537194c5517b28a519
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-10-13 17:56:43 -0300

    corregido el fourier y bajado a n=10 para que se note la diferencia con la triangular exacta

--- sim.m
+++ sim.m
@@ -1,25 +1,24 @@
 clear;
 
-aproxFourierTriangular(x,t)
 % Para una simulacion sencilla.
+L = pi;
 h = 1;
 c = 1;
-topeX = pi;
-pasoX = topeX/100;
+pasoX = L/100;
 T = (2*pi)/c;
 pasoT = T/100;
-x = [0:pasoX:topeX];
+x = [0:pasoX:L];
 
 % Para pasar handler
 f1 = estacionaria(@sin,c)
 f2 = estacionaria(@(x) triangular(x,h,pi),c)
 % Aproximacion de fourier de la triangular.
-n = [0:100]';
-f3 = @(x) sum(8*h/pi * (-1).^n ./ ((2*n + 1).^2) .* sin(pi/L*(2*n + 1)*x));
+n = [0:10]';
+f3 = @(x,t) 8*h/(pi^2)*sum((-1).^n ./ ((2.*n + 1).^2) .* sin(pi/L.*(2.*n + 1)*x) .* cos(pi/L.*(2.*n + 1).*c.*t));
 for t = [0:pasoT:T]
   plot( x, f1(x,t),x,f2(x,t),x,f3(x,t),"-");
-  axis([0,topeX,-1,1]);
-  legend("senoidal","triangular", "triangular aproximada"));
+  axis([0,L,-1,1]);
+  legend("senoidal","triangular", "triangular aproximada");
   nombreImagen = sprintf("./output/%05d.png",t/pasoT);
   print("-dpng",nombreImagen);
 endfor
