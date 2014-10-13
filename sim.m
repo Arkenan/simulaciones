bb00dd052c943304a8eda6e2a65b86c8ad514a25
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-10-13 17:42:50 -0300

    se añade la aproximacion por series de fourier hasta 100 de la triangular

--- sim.m
+++ sim.m
@@ -1,6 +1,8 @@
 clear;
 
+aproxFourierTriangular(x,t)
 % Para una simulacion sencilla.
+h = 1;
 c = 1;
 topeX = pi;
 pasoX = topeX/100;
@@ -10,12 +12,14 @@ x = [0:pasoX:topeX];
 
 % Para pasar handler
 f1 = estacionaria(@sin,c)
-f2 = estacionaria(@(x) triangular(x,1,pi),c)
+f2 = estacionaria(@(x) triangular(x,h,pi),c)
 % Aproximacion de fourier de la triangular.
-f3 = @(x) sum( 
+n = [0:100]';
+f3 = @(x) sum(8*h/pi * (-1).^n ./ ((2*n + 1).^2) .* sin(pi/L*(2*n + 1)*x));
 for t = [0:pasoT:T]
-  plot( x, f1(x,t),x,f2(x,t),"-" );
+  plot( x, f1(x,t),x,f2(x,t),x,f3(x,t),"-");
   axis([0,topeX,-1,1]);
+  legend("senoidal","triangular", "triangular aproximada"));
   nombreImagen = sprintf("./output/%05d.png",t/pasoT);
   print("-dpng",nombreImagen);
 endfor
