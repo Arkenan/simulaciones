7fb580f10509a689240a471c8d755eed17701747
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-10-15 11:57:07 -0300

    n=3 en Fourier para distinguir mejor a la verdadera triangular.

--- sim.m
+++ sim.m
@@ -1,6 +1,6 @@
 clear;
 
-% Para una simulacion sencilla.
+% Datos para una simulacion sencilla.
 L = pi;
 h = 1;
 c = 1;
@@ -10,12 +10,14 @@ pasoT = T/100;
 x = [0:pasoX:L];
 
 % Para pasar handler
-f1 = estacionaria(@sin,c)
-f2 = estacionaria(@(x) triangular(x,h,pi),c)
+f1 = estacionaria(@sin,c);
+f2 = estacionaria(@(x) triangular(x,h,pi),c);
 % Aproximacion de fourier de la triangular.
-n = [0:10]';
-f3 = @(x,t) 8*h/(pi^2)*sum((-1).^n ./ ((2.*n + 1).^2) .* sin(pi/L.*(2.*n + 1)*x) .* cos(pi/L.*(2.*n + 1).*c.*t));
-for t = [0:pasoT:T]
+n = [0:3]';
+f3 = @(x,t) 8.*h/(pi^2).*sum((-1).^n ./ ((2.*n + 1).^2) .* sin(pi/L.*(2.*n + 1)*x) .* cos(pi/L.*(2.*n + 1).*c.*t));
+
+% Tres periodos de oscilacion impresos en 100 imagenes c/u.
+for t = [0:pasoT:3*T]
   plot( x, f1(x,t),x,f2(x,t),x,f3(x,t),"-");
   axis([0,L,-1,1]);
   legend("senoidal","triangular", "triangular aproximada");
