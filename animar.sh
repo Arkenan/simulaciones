2304ef96a39db7d3093db741bb9e11ee157f4262
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-10-15 11:54:50 -0300

    Output con los pasos en los que se encuentra el script

--- animar.sh
+++ animar.sh
@@ -2,15 +2,21 @@
 
 #crea la carpeta de output.
 if [ ! -d './output' ]
-then mkdir output
+then mkdir output & echo "Creando carpeta de output..."
 fi
 
+echo "Ploteando y Guardando imágenes..."
 octave sim.m
+echo "Listo."
 
 cd output
 
+echo "Redimensionando imagenes..."
 #Todas las imagenes del mismo tamaño Y MISMO TIPO DE COLOR.
 mogrify -resize 1200X900 -define png:color-type=2 *.png
+echo "Listo."
 
+echo"Convirtiendo fotogramas a video..."
 #conversion a video
 avconv -i "%05d.png" -r 25  animacion.mp4 
+echo "Fin."
